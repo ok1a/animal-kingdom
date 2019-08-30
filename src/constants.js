670278222d36e95a4d54208471b89aebca025b6b
omar okla <omaro@me.com>
2019-08-30 00:44:15 -0400

    add new territority, and new kingdom

--- src/constants.js
+++ src/constants.js
@@ -1,56 +1,65 @@
-import { AppConfig } from 'blockstack'
+import { AppConfig } from 'blockstack';
 
-export const appConfig = new AppConfig(['store_write', 'publish_data'])
+export const appConfig = new AppConfig(['store_write', 'publish_data']);
 export const ANIMALS = [
   {
     id: 'cat',
     name: 'Cat',
-    superpower: 'Has 9 lives.'
+    superpower: 'Has 9 lives.',
   },
   {
     id: 'dog',
     name: 'Dog',
-    superpower: 'Catching frisbees.'
+    superpower: 'Catching frisbees.',
   },
   {
     id: 'lion',
     name: 'Lion',
-    superpower: 'Roaring loudly.'
-  }]
+    superpower: 'Roaring loudly.',
+  },
+];
 
 export const TERRITORIES = [
   {
     id: 'forest',
     name: 'Forest',
-    superpower: 'Trees!'
+    superpower: 'Trees!',
   },
   {
     id: 'tundra',
     name: 'Tundra',
-    superpower: 'Let it snow!'
-  }
-]
+    superpower: 'Let it snow!',
+  },
+  {
+    id: 'narnia',
+    name: 'Narnia',
+    superpower: 'Turkish Delight',
+  },
+];
 
-export const ME_FILENAME = 'me.json'
-export const SUBJECTS_FILENAME = 'subjects.json'
-export const EXPLORER_URL = 'https://explorer.blockstack.org'
-
+export const ME_FILENAME = 'me.json';
+export const SUBJECTS_FILENAME = 'subjects.json';
+export const EXPLORER_URL = 'https://explorer.blockstack.org';
 
 export const OTHER_KINGDOMS = [
   {
     app: 'https://animal-kingdom-1.firebaseapp.com',
-    ruler: 'larry.id'
+    ruler: 'larry.id',
   },
   {
     app: 'http://localhost:3001',
-    ruler: 'larz.id'
+    ruler: 'larz.id',
   },
   {
     app: 'https://decentralised-islands.netlify.com',
-    ruler: 'yannael_leborgne.id'
+    ruler: 'yannael_leborgne.id',
   },
   {
-      app: 'https://thirsty-jang-0c0a17.netlify.com',
-      ruler: 'ma1222042.id.blockstack'
-  }
-]
+    app: 'https://thirsty-jang-0c0a17.netlify.com',
+    ruler: 'ma1222042.id.blockstack',
+  },
+  {
+    app: 'https://animalkingdoms.netlify.com',
+    ruler: 'moxiegirl.id.blockstack',
+  },
+];
